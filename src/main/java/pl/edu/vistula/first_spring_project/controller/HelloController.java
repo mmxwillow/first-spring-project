727a99df744d18d69a0b6585f17224c7144b5c45
Monika <mxwierzba@gmail.com>
2025-01-18 10:44:54 +0100

    fixed (?) controller

--- src/main/java/pl/edu/vistula/first_spring_project/controller/HelloController.java
+++ src/main/java/pl/edu/vistula/first_spring_project/controller/HelloController.java
@@ -2,14 +2,12 @@ package pl.edu.vistula.first_spring_project.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 //@RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping(value ="/")
     public String hello() {
         return "Hello World";
     }
